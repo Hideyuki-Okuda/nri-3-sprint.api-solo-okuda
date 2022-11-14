8ef47d08f4d6591568a19bd541008a6c4690f212
okdhide <okuda.hideyuki.tj@gmail.com>
2022-11-14 10:33:18 +0900

    edit yaml

--- src/controller/shop.controller.js
+++ src/controller/shop.controller.js
@@ -7,20 +7,30 @@ module.exports = {
     },
 
     async view(req, res) {
-        const id = parseInt(req.params.idOrName);
-        let shops;
-        if (isNaN(id)) {
-            const name = req.params.idOrName;
-            shops = await shopModel.selectByName(name);
-        } else {
-            shops = await shopModel.selectById(id);
+        try {
+            const id = parseInt(req.params.idOrName);
+            let shops;
+            if (isNaN(id)) {
+                const name = req.params.idOrName;
+                shops = await shopModel.selectByName(name);
+            } else {
+                shops = await shopModel.selectById(id);
+            }
+            res.status(200).json(shops);
+        } catch (err) {
+            res.status(400).json({ message: err });
         }
-        res.json(shops);
     },
 
     async add(req, res) {
-        const result = await shopModel.create(req.body);
-        res.json(result);
+        try {
+            await shopModel.create(req.body);
+            res.status(201).json({
+                url: 'http://localhost:3000/shops/' + req.body.id,
+            });
+        } catch (err) {
+            res.status(400).json({ message: err });
+        }
     },
 
     async edit(req, res) {
